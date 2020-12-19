90246f190386731e36f94db0e27ce732de8c10a8
sylvanas <jzm362466464@126.com>
2020-12-19 15:44:13 +0800

    init pro

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+import Form from './Form'
+
+function App() {
+	return (
+		<Box w="500px" m="10px auto">
+			<Form />
+		</Box>
+	)
+}
+
+export default App
